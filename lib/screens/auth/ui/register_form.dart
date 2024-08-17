fe69fa701ab2e1b0625e1e424a2083515c61b6ff
Roman Honcharov <romis200786@gmail.com>
2024-08-17 14:46:13 +0300

    auth forms mock

---
+++ lib/screens/auth/ui/register_form.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class RegisterForm extends StatelessWidget {
+  RegisterForm({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  void _onSubmit() {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final localization = AppLocalizations.of(context);
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  controller: _firstNameController,
+                  decoration: InputDecoration(
+                    labelText: localization?.firstName ?? '',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return localization?.fieldRequired(
+                              localization.firstName.toLowerCase()) ??
+                          '';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: TextFormField(
+                  controller: _lastNameController,
+                  decoration: InputDecoration(
+                    labelText: localization?.lastName ?? '',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return localization?.fieldRequired(
+                              localization.lastName.toLowerCase()) ??
+                          '';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+            ],
+          ),
+          TextFormField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              labelText: localization?.email ?? '',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return localization
+                        ?.fieldRequired(localization.email.toLowerCase()) ??
+                    '';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              labelText: localization?.password ?? '',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return localization
+                        ?.fieldRequired(localization.password.toLowerCase()) ??
+                    '';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _confirmPasswordController,
+            decoration: InputDecoration(
+              labelText: localization?.confirmPassword ?? '',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return localization?.fieldRequired(
+                        localization.confirmPassword.toLowerCase()) ??
+                    '';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _onSubmit,
+            child: Text(localization?.register ?? '',
+                style: const TextStyle(fontSize: 20)),
+          ),
+        ],
+      ),
+    );
+  }
+}
